855eb69701632656b8354b27cc1d9cf16910c932
LChesani <92533013+LChesani@users.noreply.github.com>
2022-06-27 16:01:06 -0300

    Update Animalia.java

--- Animalia.java
+++ Animalia.java
@@ -33,3 +33,45 @@ class Golfinho extends Mamifero {
         return terrestre;
     }
 }
+
+class Carnivoro extends Animalia{
+    protected int n_dentes;
+    protected float tamanho_dente;
+    protected String carne_favorita;
+    protected int idade;
+    public void setIdade(int idade){
+        this.idade = idade;
+    }
+    public int getIdade(){
+        return idade;
+    }
+    public Carnivoro(){
+
+    }
+    public void setCarne_favorita(String carne_favorita){
+        this.carne_favorita = carne_favorita;
+    }
+    public String getCarne_favorita(){
+        return carne_favorita;
+    }
+    public void setN_dentes(int n_dentes){
+        this.n_dentes = n_dentes;
+    }
+    public int getN_dentes(){
+        return n_dentes;
+    }
+}
+
+class TRex extends Carnivoro{
+    Bool vivo;
+    public void setVivo(Bool vivo) {
+        this.vivo = vivo;
+    }
+    public Bool getVivo() {
+        return vivo;
+    }
+    public Trex(){
+        this.n_dentes = 60;
+        this.tamanho_dente = 0.20;
+    }
+}
