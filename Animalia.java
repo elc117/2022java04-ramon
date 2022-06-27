494abcb00380655b929e657e751e2de229936cc5
Gregori Dallanora Rubin <gregori.rubin@gmail.com>
2022-06-27 16:20:17 -0300

    Classes Ave e Pinguim adicionadas

--- Animalia.java
+++ Animalia.java
@@ -1,35 +1,78 @@
 //Pode criar novos reinos
-class Animalia{
+class Animalia {
     private String reino;
-    
+
     public Animalia() {
         reino = "animalia";
     }
-    public String getReino(){
+
+    public String getReino() {
         return reino;
     }
 }
-//Pode criar novas subclasses
-class Mamifero extends Animalia{
+
+// Pode criar novas subclasses
+class Mamifero extends Animalia {
     private int especies = 5000;
-    
-    public int quantidade_especies(){
+
+    public int quantidade_especies() {
         return especies;
     }
 }
-//Pode criar algum outro animal 
+
+// Pode criar algum outro animal
 class Golfinho extends Mamifero {
     private boolean terrestre;
     private boolean aquatico;
 
-    public Golfinho(){
+    public Golfinho() {
         this.terrestre = false;
         this.aquatico = true;
     }
-    public boolean sabe_nadar(){
+
+    public boolean sabe_nadar() {
         return aquatico;
     }
-    public boolean sabe_andar(){
+
+    public boolean sabe_andar() {
         return terrestre;
+    }
+}
+
+class Ave extends Animalia {
+    private int especies = 10000;
+    private int quant_pernas = 2;
+    protected boolean sabe_voar;
+    protected int altura_de_voo;
+
+    public boolean isSabe_voar() {
+        return sabe_voar;
+    }
+
+    public void setSabe_voar(boolean sabe_voar) {
+        this.sabe_voar = sabe_voar;
+    }
+
+    public int getAltura_de_voo() {
+        return altura_de_voo;
+    }
+
+    public void setAltura_de_voo(int altura_de_voo) {
+        this.altura_de_voo = altura_de_voo;
+    }
+
+    public Ave(boolean sabe_voar, int altura_de_voo) {
+        this.sabe_voar = sabe_voar;
+        this.altura_de_voo = altura_de_voo;
+    }
+
+    public int getEspecies() {
+        return especies;
+    }
+}
+
+class Pinguim extends Ave {
+    public Pinguim() {
+        super(true, 0);
     }
 }
